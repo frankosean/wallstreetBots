2155140ae98f6d8811726ab2c51c4d5ece1c03b7
Sean Franklin <Franklinsean@outlook.com>
2021-11-14 11:25:18 -0600

    Add files via upload

---
+++ WALLSTREETBOTS/config.py
@@ -0,0 +1,8 @@
+API_URL = 'https://paper-api.alpaca.markets'
+API_KEY = 'PKT3JF4ZD25C7MW0C5P5'
+API_Secret = 'tfMYMhJ8Ouo9CEbOXLqeNIBQvH6jINuNhfxrWyFb'
+
+DB_HOST = 'localhost'
+DB_USER = 'postgres'
+DB_PASS = 'password'
+DB_NAME = 'etfdb'
